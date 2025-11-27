b553b1ffd46dddfa37a353a011fa1339dcfc6a55
GijaWunder <20251foz0020019@estudantes.ifpr.edu.br>
2025-11-27 11:48:27 -0300

    Update surpresinha.php

--- surpresinha.php
+++ surpresinha.php
@@ -21,37 +21,37 @@
 
             switch($opcao){
                 case "1":
-                    print("Você escolheu a \033[37;42mMega Sena.\033[0m\n");
+                    print("Você escolheu a \033[37;42mMega Sena.\033[0m\n\n");
                     megaSena();
 
                     break;
 
                 case "2":
-                    print("Você escolheu a \033[37;42mQuina.\033[0m\n");
-                    //quina();
+                    print("Você escolheu a \033[37;42mQuina.\033[0m\n\n");
+                    quina();
 
                     break;
 
                 case "3":
-                    print("Você escolheu a \033[37;42mLotofácil.\033[0m\n");
-                    //lotofacil();
+                    print("Você escolheu a \033[37;42mLotofácil.\033[0m\n\n");
+                    lotofacil();
 
                     break;
 
                 case "4":
-                    print("Você escolheu a \033[37;42mLotomania.\033[0m\n");
-                    //lotomania();
+                    print("Você escolheu a \033[37;42mLotomania.\033[0m\n\n");
+                    lotomania();
 
                     break;
 
                 case "0";
-                    print("Você escolheu sair.\n");
+                    print("Você escolheu sair.\n\n");
                     $continuar = false;
                     break;
 
                 default:
 
-                    print("Opção inválida!\n");
+                    print("Opção inválida!\n\n");
                     break;
 
             }
@@ -60,15 +60,36 @@
 
     }
 
-    //menu();
-    megaSena();
+    menu();
 
     function megaSena(): void{
 
-        //$quantidadeJogos = readline("Quantos jogos deseja?\n");
-        $quantidadeDezena = readline("Quantas dezenas para cada jogo?\n");
-        minimoEmaximo($quantidadeDezena, 6, 20, 1, 60);
+        $quantidadeJogos = readline("Quantos jogos deseja?\n");
+        jogos($quantidadeJogos, 6, 20, 1, 60);
 
+    }
+
+    function quina(): void{
+
+        $quantidadeJogos = readline("Quantos jogos deseja?\n");
+        jogos($quantidadeJogos, 5, 15, 1, 80);
+
+    }
+
+    function lotofacil(): void{
+
+        $quantidadeJogos = readline("Quantos jogos deseja?\n");
+        jogos($quantidadeJogos, 15, 20, 1, 25);
+
+    }
+
+    function lotomania(){
+
+        $quantidadeDezena = 50;
+        print("Nesta loteria a quantidade de dezenas é fixa em 50 dezenas.\n");
+        $quantidadeJogos = readline("Quantos jogos deseja?\n");
+        jogosLotomania($quantidadeJogos, 50, 0, 99);
+        
     }
 
     function sorteio($minimo, $maximo, $quantidadeDezena){
@@ -86,7 +107,7 @@
 
         sort($sorteados);
         
-        print implode(", ", $sorteados);
+        print implode(" | ", $sorteados);
 
     }
 
@@ -94,14 +115,14 @@
 
          if ($quantidadeDezena < $minimoDezena) {
            while ($quantidadeDezena < $minimoDezena) {
-                $quantidadeDezena = (int) readline("Valor baixo! Digite novamente: \n");
+                $quantidadeDezena = (int) readline("Valor baixo! Digite novamente: \n\n");
 
             }
             sorteio($minimoUniverso, $maximoUniverso, $quantidadeDezena);
 
         }elseif ($quantidadeDezena > $maximoDezena) {
             while ($quantidadeDezena > $maximoDezena) {
-                $quantidadeDezena = (int) readline("Valor alto! Digite novamente: \n");
+                $quantidadeDezena = (int) readline("Valor alto! Digite novamente: \n\n");
                 
             }
             sorteio($minimoUniverso, $maximoUniverso, $quantidadeDezena);
@@ -109,4 +130,21 @@
         }else{
             sorteio($minimoUniverso, $maximoUniverso, $quantidadeDezena);
         }
+    }
+
+    function jogos($quantidadeJogos, $minimoDezena, $maximoDezena, $minimoUniverso, $maximoUniverso){
+        for ($i=0; $i < $quantidadeJogos; $i++) { 
+            $quantidadeDezena = (int) readline('Quantas dezenas deseja para o jogo ' . $i + 1 . "? \n");
+            minimoEmaximo($quantidadeDezena, $minimoDezena, $maximoDezena, $minimoUniverso, $maximoUniverso);
+            print("\n\n");
+        }
+    }
+
+    function jogosLotomania($quantidadeJogos, $quantidadeDezena, $minimoUniverso, $maximoUniverso){
+        for ($i=0; $i < $quantidadeJogos; $i++) {
+            print('Jogo ' . $i + 1 . ": \n");
+            sorteio(0, 99, 50);
+            print("\n\n");
+        }
+        
     }
